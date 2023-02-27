a5204b69ed416b0eed29a1abe0ec27e3465ae8eb
Fireball-76 <126358249+Fireball-76@users.noreply.github.com>
2023-02-27 21:38:39 +0300

    -js

--- skyrim_prima/prima.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-var dropdowns = Array();
-window.addEvent("domready", function(){	
-	if($$('.dropdown').length > 0){
-		$$('.dropdown').each(function(dd){
-			dropdowns.push(new elSelect({container : dd.get("id")}));
-		});
-	}
-});
